313c9ddcaccafde9f0fd32f244372ede1b0a5c3f
AlanNabiev <alannabiev2001@gmail.com>
2021-10-03 17:46:25 +0300

    feat: primary layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
@@ -13,6 +14,7 @@ import { InputComponent } from './shared/input/input.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     environment.production ? [] : AkitaNgDevtools.forRoot()
   ],
   providers: [],
